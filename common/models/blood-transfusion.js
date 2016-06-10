f7fd633119f0e86de4e08961c4eeb1e1e46ac79b
Gustavo RPS <email@gustavorps.net>
2016-06-09 23:06:07 -0300

    implemented Twilio sms send when do/crate a bood transfusion

--- common/models/blood-transfusion.js
+++ common/models/blood-transfusion.js
@@ -5,4 +5,20 @@ module.exports = function(BloodTransfusion) {
   // BloodTransfusion.createFakeData = function (faker) {
   //   return null;
   // };
+
+  BloodTransfusion.observe('after save', function(ctx, next) {
+    if (ctx.instance) {
+      app.models.Twilio.send({
+        type: 'sms',
+        to: ctx.instance.donor.phone,
+        from: process.env.TWILIO_FROM,
+        body: 'A instituição X tem a felicidade de informar que o sangue que ' +
+              'você  doou ajudou a salvar mais uma vida! 😄\n' +
+              'Agracemos muito pela sua ação de bondade e desejamos muita saúde e paz.'
+      }, function(err, resp) {
+        if (err) next(err);
+        else next(null, resp);
+      });
+    }
+  });
 }
--- server/datasources.local.js
+++ server/datasources.local.js
@@ -1,11 +1,22 @@
 var MONGODB_URL = process.env.MONGODB_URL || null;
 
+var datasources = {};
+
 if (MONGODB_URL) {
-  module.exports = {
-    db: {
-      name: 'db',
-      connector: 'loopback-connector-mongodb',
-      url: MONGODB_URL
-    }
+  datasources.db = {
+    name: 'db',
+    connector: 'loopback-connector-mongodb',
+    url: MONGODB_URL
   };
 }
+
+datasources.twilio = {
+  name: 'twilio',
+  connector: 'loopback-connector-twilio',
+  accountSid: process.env.TWILIO_ACCOUNT_SID,
+  authToken: process.env.TWILIO_AUTH_TOKEN
+};
+
+console.log(process.env.TWILIO_ACCOUNT_SID)
+
+module.exports = datasources;
