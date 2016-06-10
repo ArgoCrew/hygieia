2e0d161180273ac5e0361ed0b0f308dfed36e05d
Gustavo RPS <email@gustavorps.net>
2016-06-09 23:05:09 -0300

    started model for Blood Transfusion

---
+++ common/models/blood-transfusion.js
@@ -0,0 +1,8 @@
+'use strict';
+var app = require('../../server/server');
+
+module.exports = function(BloodTransfusion) {
+  // BloodTransfusion.createFakeData = function (faker) {
+  //   return null;
+  // };
+}
